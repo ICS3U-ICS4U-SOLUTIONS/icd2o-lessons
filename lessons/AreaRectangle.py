b265f276b62d4a9946b725e7b97e713a2cadac7b
e28166 <e28166@DESKTOP-56C86U1>
2025-11-13 12:28:52 -0500

    asdfdsf

---
+++ lessons/AreaRectangle.py
@@ -0,0 +1,17 @@
+# variables
+length = 0
+width = 0
+answer = 0
+
+# Input
+print("Welcome to Area of Rectangle Calculator.")
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+
+# Processing
+area = length * width
+
+# Output
+print("The area of the rectangle is: " + str(area) + " squared")
+
+

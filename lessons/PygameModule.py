5b3b727abdf1a4cf8a73283105defcbf3539ddce
e28166 <e28166@DESKTOP-56C86U1>
2025-11-18 12:40:51 -0500

    s

--- lessons/PygameModule.py
+++ lessons/PygameModule.py
@@ -1,7 +1,7 @@
 import pygame
 
 # importing sub-modules
-from pygame import Color, rect
+from pygame import Color, Rect
 from pygame import draw
 from pygame import display
 
@@ -16,6 +16,12 @@ gameDisplay = display.set_mode(SCREEN_SIZE)
 
 # background - color of the sky
 gameDisplay.fill(Color('lightblue'))
+
+# draw a box for a house
+draw.rect(gameDisplay, Color('brown'), Rect(100, 200, 300, 200))
+
+# draw a triangle for a roof
+draw.polygon(gameDisplay, Color('black'), [(100, 200), (400, 200), (250, 50)])
 
 
 # show the graphical display on the screen

20b3084232e51b8afede0f2a94982b87f64dbc31
e28166 <e28166@DESKTOP-56C86U1>
2025-11-19 12:00:36 -0500

    s

--- lessons/PygameModule.py
+++ lessons/PygameModule.py
@@ -23,6 +23,11 @@ draw.rect(gameDisplay, Color('brown'), Rect(100, 200, 300, 200))
 # draw a triangle for a roof
 draw.polygon(gameDisplay, Color('black'), [(100, 200), (400, 200), (250, 50)])
 
+# draw green grass
+draw.rect(gameDisplay, Color('green'), Rect(0, 400, 500, 100))
+
+# draw a sun
+draw.circle(gameDisplay, Color('yellow'), (50, 50), 50)
 
 # show the graphical display on the screen
 display.flip()
